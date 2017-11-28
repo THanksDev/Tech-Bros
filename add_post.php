f9da1484a971bc7bc1645c902d3b8de7bf9deda5
THanksDev <31476864+THanksDev@users.noreply.github.com>
2017-11-27 23:50:30 -0500

    Add files via upload

--- add_post.php
+++ add_post.php
@@ -14,15 +14,15 @@
     <header>
         <div class="container">
             <div id="brand">
-                <h1>Tech <span>Bros</span></h1>
+<!-- May Change to Logo Later -->
+                <a href="index.php"><h1>Tech <span>Bros</span></h1></a>
             </div>
             <nav>
                 <ul>
                     <li><a href="../index.php">Home</a></li>
-<!--
+<!-- These two link need to not be visible unless you are logged in to the admin panel -->
                     <li><a href="add_post.php">Post</a></li>
                     <li><a href="edit_post.php">Edit</a></li>
--->
                 </ul>
             </nav>
         </div>
@@ -35,20 +35,43 @@
 -->
     <div id="main">
         <div class="container">
-                
+            
+            
+            <?
+            if(!empty($_POST)) {
+	           include '../mysql.php';
+	           if(mysql_safe_query('INSERT INTO posts (title,body,caption,date) VALUES (%s,%s,%s,%s)', $_POST['title'], $_POST['body'], $_POST['caption'], time()))
+// Need to make this an alert message using html and css
+		          echo ('Entry posted!');
+// End the alert message here
+	           else
+		          echo mysql_error();
+            }
+            ?>
+
+<!-- Need this to be styled completely however you want -->
             <form method="post">
 	           <table>
 		          <tr>
-			         <td><label for="username">Username</label></td>
-			         <td><input name="username" id="username" /></td>
+			         <td><label for="title">Title</label></td>
+			         <td><input name="title" id="title" /></td>
+		          </tr>
+                  <tr>
+<!-- The upload image function of the posting has not been completed yet -->
+			         <td><label for="img">Upload Image</label></td>
+			         <td><input type="file" name="img" id="img" /></td>
+		          </tr>
+                  <tr>
+			         <td><label for="caption">Image Caption</label></td>
+			         <td><input name="caption" id="caption" /></td>
 		          </tr>
 		          <tr>
-			         <td><label for="password">Password</label></td>
-			         <td><input type="password" name="password" id="password" /></td>
+			         <td><label for="body">Body</label></td>
+			         <td><textarea name="body" id="body"></textarea></td>
 		          </tr>
 		          <tr>
 			         <td></td>
-			         <td><input type="submit" value="Login" /></td>
+			         <td><input type="submit" value="Post" /></td>
 		          </tr>
 	           </table>
             </form>
